5aa7810a7e373fdc677f590f36a726a8adcf2f61
unknownicse27 <wecolor33@naver.com>
2026-07-04 00:21:26 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import find_packages
 from setuptools import setup
-
 from reuse import __version__
-
 
 setup(
     name='reuse',
     version=__version__,
-    description='reuse: Maximizing the Power of Symbolic Execution by Adaptively Tuning External Parameters',
-    python_version='>=3.9',
+    description='ReuSE: Refining Query Results for Efficient Symbolic Execution Across Software Releases',
+    python_requires='>=3.9',
     packages=find_packages(include=('reuse', 'reuse.*')),
     include_package_data=True,
     setup_requires=[],
-    install_requires=[],
+    install_requires=[
+        'zstandard>=0.21.0',
+        'z3-solver>=4.12.0',
+    ],
     dependency_links=[],
     entry_points={
         'console_scripts': [
